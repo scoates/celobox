78199656f3bd17ab74d343d8516b48fba2c797a2
Sean Coates <sean@seancoates.com>
2017-02-07 14:20:38 -0500

    rework phantom (and real browser if necessary), move back to passwd.py

--- passwd.py
+++ passwd.py
@@ -1,21 +1,42 @@
 #!/usr/bin/env python
+
+from selenium import webdriver
 import sys
 import json
 import logging
 import getpass
+import os
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from enum import Enum
+import time
 import requests
-from bs4 import BeautifulSoup
+import yaml
 
 
-class PasswdDomainException(Exception):
+USER_AGENT = os.environ.get("USER_AGENT", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36")
+
+class PasswdDomainError(Exception):
     pass
 
 
-class Passwd:
-    def __init__(self, domain, debug=False):
+class ContentTypes(Enum):
+    """Enumerate possible content type hints."""
+
+    JSON = 'json'
+    YAML = 'yaml'
+
+
+class Passwd(object):
+
+    def __init__(self, domain, debug=False, service_args=[], ignore_ssl_errors=False):
         self.domain = domain
-        self.data = self.load_data(domain)
-        self.csrf_token = None
+        self.ignore_ssl_errors = ignore_ssl_errors
+        self.service_args = service_args
+        if ignore_ssl_errors:
+            self.service_args = (self.service_args or []) + ['--ignore-ssl-errors=yes']
         if debug:
             loglevel = logging.DEBUG
         else:
@@ -23,69 +44,156 @@ class Passwd:
         logging.basicConfig(level=loglevel)
         self.logger = logging.getLogger(self.__class__.__name__)
         self.logger.debug('Logging enabled')
-        self.new_session()
 
-    def new_session(self):
-        self.session = requests.Session()
-        self.session.headers.update(self.data.get('headers', {}))
+    def __enter__(self):
+        dcap = dict(DesiredCapabilities.PHANTOMJS)
+        dcap["phantomjs.page.settings.userAgent"] = USER_AGENT
+        self.phantom_driver = webdriver.PhantomJS(desired_capabilities=dcap, service_args=self.service_args)
+        self.phantom_driver.set_window_size(1024, 768)
+
+        # default to the phantom driver
+        # we do this before load_data because load_data does a GET to attempt to load the manifest
+        self.driver = self.phantom_driver
+
+        self.data = self.load_data(self.domain)
+        if self.data and self.data.get('needs_real_browser', False):
+            # load the *real* driver, but only if we have to (use phantom otherwise)
+            self.real_driver = webdriver.Chrome()
+            self.real_driver.set_window_size(1024, 768)
+
+            self.driver = self.real_driver
+        else:
+            self.real_driver = None
+
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.phantom_driver.quit()
+        if self.real_driver:
+            self.real_driver.quit()
+
+    @property
+    def throttle(self):
+        return self.data.get('throttle', 0)
+
+    def load_manifest(self, domain):
+        self.phantom_driver.get("http://{domain}".format(domain=domain))
+        try:
+            elt = self.phantom_driver.find_element_by_css_selector('link[rel="password-manifest"]')
+            resp = requests.get(elt.get_attribute('href'), verify=not self.ignore_ssl_errors)
+            return self.load_data_from_content(resp.content)
+        except NoSuchElementException:
+            return {}
 
     def load_data(self, domain):
-        try:
-            return json.load(open('manifests/%s.json' % domain))
-        except (IOError):
-            raise PasswdDomainException
+        manifests_path = self._get_manifests_path()
+        domain = self._get_safe_domain(domain)
 
-    def get_csrf(self, page_data, action):
-        # use the action-specific csrf selector if possible
-        if 'csrf_token' in self.data[action]:
-            csrf_token = self.data[action]['csrf_token']
-        else:
-            csrf_token = self.data['csrf_token']
-        bs = BeautifulSoup(page_data)
-        el = bs.select(csrf_token['selector'])[0]
-        token = el[csrf_token['attribute']]
-        self.logger.debug('Found CSRF token: %s' % token)
-        return token
+        for content_type in ContentTypes:
+            try:
+                content = open(os.path.join(manifests_path, '{}.{}'.format(domain, content_type.value)))
+                return self.load_data_from_content(content, content_type)
+            except IOError:
+                # file unreadable (not found)
+                pass
 
-    def test_success(self, post, container):
-        if 'header-present' == container['success']['test']:
-            return container['success']['name'] in post.headers
+        # have not yet found a local manifest, so try to load via web
+        return self.load_manifest(domain)
 
-        if 'landing' == container['success']['test']:
-            return container['success']['name'] == post.request.url
 
-        if 'page' == container['success']['test']:
-            r = self.session.get(container['success']['name'])
-            return r.status_code == requests.codes.ok
+    def load_data_from_content(self, content, hint=None):
+        if hint is None or hint == ContentTypes.JSON:
+            try:
+                return json.load(content)
+            except ValueError:
+                if hint == ContentTypes.JSON:
+                    return False
+                # otherwise: pass through to other parsers
+
+        if hint is None or hint == ContentTypes.YAML:
+            try:
+                parsed = yaml.safe_load(content)
+                # most strings parse to be valid YAML (it's very permissive)
+                # so, let's check a known key to be sure.
+                if parsed and 'celobox_manifest' in parsed:
+                    return parsed
+                if hint == ContentTypes.YAML:
+                    # no key in expected-to-be-YAML content
+                    return False
+                # otherwise: pass through (this is not YAML)
+            except yaml.YAMLError:
+                if hint == ContentTypes.YAML:
+                    return False
+                # otherwise: pass through to other parsers
+
+        # out of parsers; no return yet, means we couldn't parse the content
+        return False
+
+
+    def _get_manifests_path(self):
+        """ todo """
+        return 'manifests'
+
+
+    def _get_safe_domain(self, domain):
+        """ todo: avoid "/path/to/somesecret" or "../../etc/whatever" """
+        return domain
+
+
+    def test_success(self, container):
+        if 'landing' in container['success']:
+            return container['success']['landing'] == self.driver.current_url
 
         raise ValueError("Unknown login verification test")
 
+    def click_login_url(self):
+        for a in self.driver.find_elements_by_tag_name('a'):
+            text = a.text.lower()
+            if 'log in' in text or 'login' in text or 'sign in' in text:
+                self.driver.get(a.get_attribute('href'))
+                return True
+        return False
+
+    def find_login_form(self):
+        for form in self.driver.find_elements_by_tag_name('form'):
+            text_input = form.find_elements_by_css_selector('input[type="text"]')
+            password_input = form.find_elements_by_css_selector('input[type="password"]')
+
+            if len(text_input) == 1 and len(password_input) == 1:
+                return form
+
+    def heuristic_sign_in(self, username, password):
+        self.driver.get('http://{domain}'.format(domain=self.domain))
+        time.sleep(3)
+        login_clicked = self.click_login_url()
+        if login_clicked:
+            time.sleep(3)
+            url = self.driver.current_url
+            form = self.find_login_form()
+            if form:
+                form.find_element_by_css_selector('input[type="text"]').send_keys(username)
+                form.find_element_by_css_selector('input[type="password"]').send_keys(password)
+                form.find_element_by_css_selector('input[type="password"]').submit()
+                time.sleep(3)
+                return url != self.driver.current_url
+
+        return False
+
     def sign_in(self, username, password):
-        self.new_session()
         self.logger.debug('Signing in: %s' % username)
+        login_data = self.data.get('login')
+        if login_data is None:
+            success = self.heuristic_sign_in(username, password)
+        else:
+            self.driver.get(login_data['url'])
+            time.sleep(self.throttle)
+            self.driver.find_element_by_css_selector(login_data['username']).send_keys(username)
+            self.driver.find_element_by_css_selector(login_data['password']).send_keys(password)
 
-        verify_ssl = self.data.get('verify_ssl', True)
+            self.driver.find_element_by_css_selector(login_data['submit']).submit()
+            time.sleep(self.throttle)
 
-        form_page = self.session.get(
-            self.data['login']['urls']['form'], verify=verify_ssl).content
-
-        self.session.headers.update(
-            {'referer': self.data['login']['urls']['form']})
-
-        payload = dict({
-            self.data['login']['form']['username']: username,
-            self.data['login']['form']['password']: password,
-        }.items() + self.data['login']['form'].get('literal', {}).items())
-        if 'csrf' in self.data['login']['form']:
-            csrf = self.get_csrf(form_page, 'login')
-            payload = dict(payload.items() + {
-                self.data['login']['form']['csrf']: csrf}.items())
-
-        post = self.session.post(
-            self.data['login']['urls']['post'],
-            data=payload,
-            verify=verify_ssl)
-        success = self.test_success(post, self.data['login'])
+            success = self.test_success(self.data['login'])
         if success:
             self.username = username
             self.old_pass = password
@@ -93,41 +201,28 @@ class Passwd:
 
     def change_password(self, password):
         self.logger.debug('Changing password.')
-        form_page = self.session.get(
-            self.data['password']['urls']['form']).content
+        password_data = self.data['password']
+        self.driver.get(password_data['url'])
 
-        if 'literal' in self.data['password']['form']:
-            payload = self.data['password']['form']['literal']
-        else:
-            payload = {}
+        time.sleep(self.throttle)
+        if 'old_password' in password_data:
+            self.driver.find_element_by_css_selector(password_data['old_password']).send_keys(self.old_pass)
+        self.driver.find_element_by_css_selector(password_data['new_password']).send_keys(password)
+        if 'verify_password' in password_data:
+            self.driver.find_element_by_css_selector(password_data['verify_password']).send_keys(password)
 
-        for field in self.data['password']['form']['new_password']:
-            payload[field] = password
+        self.driver.find_element_by_css_selector(password_data['submit']).submit()
+        time.sleep(self.throttle)
 
-        if 'old_password' in self.data['password']['form']:
-            payload = dict(payload.items() + {
-                self.data['password']['form']['old_password']: self.old_pass
-                }.items())
+        self.driver.delete_all_cookies()
 
-        if 'csrf' in self.data['password']['form']:
-            csrf = self.get_csrf(form_page, 'password')
-            payload = dict(payload.items() + {
-                self.data['password']['form']['csrf']: csrf}.items())
-
-        self.session.headers.update(
-            {'referer': self.data['password']['urls']['form']})
-
-        self.session.post(
-            self.data['password']['urls']['post'], data=payload)
         success = self.sign_in(self.username, password)
         if success:
             self.old_pass = password
         return success
 
-
-if __name__ == "__main__":
-
-    from argparse import ArgumentParser
+def main():
+    from argparse import ArgumentParser, REMAINDER
 
     parser = ArgumentParser()
     parser.add_argument("domain", help="domain name of app")
@@ -140,47 +235,54 @@ if __name__ == "__main__":
     parser.add_argument("--username", help="Username (avoids prompt)")
     parser.add_argument("--oldpass", help="Old Password (avoids prompt)")
     parser.add_argument("--newpass", help="New Password (avoids prompt)")
+    parser.add_argument("--ignore-ssl-errors", help="Ignore SSL certificate errors", action="store_true")
+    parser.add_argument("--phantom", help="PhantomJS arguments (use this last; it gobbles up the remainder)", nargs=REMAINDER)
     args = parser.parse_args()
 
-    try:
-        passwd = Passwd(args.domain, debug=args.debug)
-    except (PasswdDomainException):
-        print "Invalid domain"
-        sys.exit(255)
+    with Passwd(args.domain, debug=args.debug, service_args=args.phantom, ignore_ssl_errors=args.ignore_ssl_errors) as passwd:
+        if args.username:
+            username = args.username
+            print "Using provided username"
+        else:
+            username = raw_input('Username: ')
 
-    if args.username:
-        username = args.username
-        print "Using provided username"
-    else:
-        username = raw_input('Username: ')
+        if args.oldpass:
+            old_pass = args.oldpass
+            print "Using provided password"
+        else:
+            old_pass = getpass.getpass('Old password: ')
 
-    if args.oldpass:
-        old_pass = args.oldpass
-        print "Using provided password"
-    else:
-        old_pass = getpass.getpass('Old password: ')
-
-    if passwd.sign_in(username, old_pass):
-        print "Sign in success."
-    else:
-        print "Sign in failed."
-        sys.exit(1)
-
-    if args.nochange:
-        sys.exit(0)
-
-    if args.newpass:
-        new_pass = args.newpass
-    else:
-        while True:
-            new_pass = getpass.getpass('New password: ')
-            new_pass2 = getpass.getpass('New password (again): ')
-            if new_pass == new_pass2:
-                break
+        try:
+            if passwd.sign_in(username, old_pass):
+                print "Sign in success."
             else:
-                print "Passwords do not match."
+                print "Sign in failed."
+                sys.exit(1)
+        except NoSuchElementException as e:
+            with open('screen.png', 'w') as f:
+                from base64 import b64decode
+                f.write(b64decode(e.screen))
+                raise e
 
-    if passwd.change_password(new_pass):
-        print "Password changed!"
-    else:
-        print "Password change failed."
+        if args.nochange:
+            sys.exit(0)
+
+        if args.newpass:
+            new_pass = args.newpass
+        else:
+            while True:
+                new_pass = getpass.getpass('New password: ')
+                new_pass2 = getpass.getpass('New password (again): ')
+                if new_pass == new_pass2:
+                    break
+                else:
+                    print "Passwords do not match."
+
+        if passwd.change_password(new_pass):
+            print "Password changed!"
+        else:
+            print "Password change failed."
+
+
+if __name__ == "__main__":
+    main()
