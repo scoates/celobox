2d43f63d7370341ceab22a5d247314de37d7164e
Sean Coates <sean@seancoates.com>
2017-04-19 10:44:54 -0400

    rework tests; prep for password changes too

--- test_celo.py
+++ test_celo.py
@@ -3,23 +3,57 @@ from passwd import Passwd
 import json
 import os
 import pytest
+from shutil import copyfile
 
 root_dir = os.path.dirname(__file__)
 
-domains = []
-for (dirpath, dirnames, filenames) in os.walk(os.path.join(root_dir, "manifests")):
-    domains.extend(['.'.join(f.split('.')[:-1]) for f in filenames])
-    break  # to prevent recursion
+data = {
+    'credentials_filename': os.path.join(root_dir, "test_credentials.json"),
+    'domains': [],
+    'credentials': [],
+    'wrote_backup': False,
+}
 
-creds_file = os.path.join(root_dir, "test_credentials.json")
-credentials = json.load(file(creds_file))
+def _get_domains():
+    for (dirpath, dirnames, filenames) in os.walk(os.path.join(root_dir, "manifests")):
+        data['domains'].extend(['.'.join(f.split('.')[:-1]) for f in filenames])
+        break  # to prevent recursion
 
-@pytest.mark.parametrize('domain', domains)
+def _get_credentials():
+    data['credentials'] = json.load(open(data['credentials_filename']))
+
+
+def _do_backup():
+    if data['wrote_backup']:
+        return
+    copyfile(data['credentials_filename'], data['credentials_filename'] + ".bak")
+
+
+def _write_credentials():
+    _do_backup()
+    data['credentials'] = json.dump(open(data['credentials_filename'], 'w'))
+
+
+
+_get_domains()
+_get_credentials()
+
+@pytest.mark.parametrize('domain', data['domains'])
 def test_has_credentials(domain):
-     assert domain in credentials, "No credentials for domain {}".format(domain)
+     assert domain in data['credentials'], "No credentials for domain {}".format(domain)
 
-@pytest.mark.parametrize('domain', credentials.keys())
+
+@pytest.mark.parametrize('domain', data['credentials'].keys())
 def test_login(domain):
-    creds = credentials[domain]
+    creds = data['credentials'][domain]
     with Passwd(domain) as passwd:
         assert passwd.sign_in(creds['user'], creds['pass']), "Could not sign in to {}".format(domain)
+
+@pytest.mark.parametrize('domain', data['credentials'].keys())
+def test_fail_login(domain):
+    creds = data['credentials'][domain]
+    with Passwd(domain) as passwd:
+        assert not passwd.sign_in(creds['user'], 'notmypassword'), "Could sign in to {} with invalid password".format(domain)
+
+# @pytest.mark.parametrize('domain', data['credentials'].keys())
+# def test_change(domain):
